578f577ec9c46579acf3be97028729b8d71f54bd
syq1874 <1953745128@qq.com>
2020-08-15 19:29:12 +0800

    代码的优化

--- S3-springmvc/src/main/java/com/yc/springmvc/web/ProductAction.java
+++ S3-springmvc/src/main/java/com/yc/springmvc/web/ProductAction.java
@@ -11,55 +11,53 @@ import com.yc.damai.bean.DmUser;
 
 @RestController
 public class ProductAction {
-		
-	
-	@RequestMapping(path = "product.do",params = "op=query")
+
+	@RequestMapping(path = "product.do", params = "op=query")
 	public String query() {
-		return "query!";
+		return "query!!";
 	}
-	
-	@RequestMapping(path = "product.do",params = "op=add")
+
+	@RequestMapping(path = "product.do", params = "op=add")
 	public String add() {
-		return "add!";
+		return "add!!";
 	}
-	
-	@RequestMapping(path = "product.do",params = "op=mod",method=RequestMethod.POST)
+
+	@RequestMapping(path = "product.do", params = "op=mod", method = RequestMethod.POST)
 	public String mod() {
-		return "mod!";
+		return "mod!!";
 	}
-	
-	@GetMapping(path = "product.do",params = "op=select")
+
+	@GetMapping(path = "product.do", params = "op=select")
 	public String select() {
-		return "select!";
+		return "select测试!!";
 	}
-	
-	@GetMapping(path = "product.do",params = "op=mgr",headers = {"Host=localhost","Cookie"})
+
+	@GetMapping(path = "product.do", params = "op=mgr", 
+			headers = { "Host=localhost", "Cookie" })
 	public String mgr() {
-		return "mgr!";
+		return "测试管理!!";
 	}
-	
 	
 	@GetMapping("login.do")
-	public String login(String user,String pwd) {
-		return user+":"+pwd;
+	public String login(String user, String pwd) {
+		return user + ":" + pwd;
 	}
 	
 	@GetMapping("reg.do")
-	public String login(DmUser user,DmCart dc) {
-		return user.toString()+"<br>"+dc;
+	public String login(DmUser user, DmCart dc) {
+		return user.toString() + "<br>" + dc;
 	}
 	
 	@GetMapping("pay.do")
 	public String pay(
 			@RequestParam(
-					name="userid",
-					//defaultValue = "1",
-					required = true
-					)
-			String uid,Double money) {
-		return uid+":"+money;
+				name="userid",	//请求参数名
+				//defaultValue="1",//默认值
+				required=true  // 必须提供
+			) 
+			String uid, Double money) {
+		return uid + ":" + money;
 	}
-	
 	
 	@GetMapping("findUser.do")
 	public DmUser findUser() {
@@ -69,8 +67,6 @@ public class ProductAction {
 		du.setPassword("123");
 		return du;
 	}
-	
-	
 	
 	
 	
