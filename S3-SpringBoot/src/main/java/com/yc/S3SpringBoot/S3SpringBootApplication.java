fc07891aa89f8e3057dfb448a10bf43fc6f4a1bc
syq1874 <1953745128@qq.com>
2020-08-20 09:57:39 +0800

    初始化工程及Hello World

---
+++ S3-SpringBoot/src/main/java/com/yc/S3SpringBoot/S3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yc.S3SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3SpringBootApplication.class, args);
+	}
+
+}
---
+++ S3-SpringBoot/src/test/java/com/yc/S3SpringBoot/S3SpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yc.S3SpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S3SpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
