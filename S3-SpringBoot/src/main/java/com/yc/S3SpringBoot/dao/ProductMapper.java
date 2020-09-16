e5d31c85b16737948108218872d0f8dd31a393d6
syq1874 <1953745128@qq.com>
2020-09-16 18:44:24 +0800

    全部项目内容

--- S3-SpringBoot/src/main/java/com/yc/S3SpringBoot/dao/ProductMapper.java
+++ S3-SpringBoot/src/main/java/com/yc/S3SpringBoot/dao/ProductMapper.java
@@ -10,5 +10,17 @@ public interface ProductMapper {
 	
 	@Select("select * from dm_product")
 	List<DmProduct> selectAll();
+	
+	
+	@Select("select * from dm_product where is_hot=1 limit 0,10")
+	List<DmProduct> selectByHot();
+	
+	
+	@Select("select * from dm_product order by createtime desc limit 0,10")
+	List<DmProduct> selectByNew();
+	
+	
+	@Select("select * from dm_product where id=#{id}")
+	DmProduct selectById(int id);
 
 }
